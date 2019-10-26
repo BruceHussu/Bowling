454e159f0f1db480053c74fecfdeb7e62afee333
BruceHussu <44323296+BruceHussu@users.noreply.github.com>
2019-10-25 23:57:48 -0400

    Delete main.js

--- js/main.js
+++
@@ -1,9 +0,0 @@
-window.onload = () => {
-    'use strict';
-
-    console.log('register service worker');
-    if ('serviceWorker' in navigator) {
-        navigator.serviceWorker
-            .register('/Bowling/sw.js');
-    }
-}
